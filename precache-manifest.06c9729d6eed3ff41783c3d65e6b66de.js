81a8bbdcafe61d9c69b98349f9f798d9f3fe80b5
Abdelhalim Khouas <khouasaeh@gmail.com>
2019-06-21 03:38:21 +0100

    add build

---
+++ precache-manifest.06c9729d6eed3ff41783c3d65e6b66de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530153d454b799739a551523ff8362db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240624b49977026ac584",
+    "url": "/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "8d631ba7c43e35a25230",
+    "url": "/static/css/main.9ad52817.chunk.css"
+  },
+  {
+    "revision": "240624b49977026ac584",
+    "url": "/static/js/2.9999a445.chunk.js"
+  },
+  {
+    "revision": "8d631ba7c43e35a25230",
+    "url": "/static/js/main.3fb02b12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
